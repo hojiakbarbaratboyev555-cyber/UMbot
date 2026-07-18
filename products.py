14e50e307776be84054f166dc5b7181f5bd771eb
hojiakbarbaratboyev555-cyber <hojiakbarbaratboyev555@gmail.com>
2026-07-18 18:46:14 +0500

    Update products.py

--- products.py
+++ products.py
@@ -1,8 +1,8 @@
 PRODUCTS = [
     {
-        "name": "1 oylik telegram premium",
-        "description": "1 oy telegram premium funksiyalaridan erkin foydlanasiz. Akkauntga kirib olib beriladi",
-        "price": 1.3,  # so'mda
+        "𝗡𝗼𝗺𝗶": "1 oylik telegram premium",
+        "𝗠𝗮ʼ𝗹𝘂𝗺𝗼𝘁": "1 oy telegram premium funksiyalaridan erkin foydlanasiz. Akkauntga kirib olib beriladi",
+        "𝗡𝗮𝗿𝘅𝗶": 1.3,  # so'mda
         "photo_file_id": None,
     },
 #    {
