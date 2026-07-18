c942d6f80d69e7ec9b668a9a415ed756fc636a4e
hojiakbarbaratboyev555-cyber <hojiakbarbaratboyev555@gmail.com>
2026-07-18 18:45:13 +0500

    Update support.py

--- support.py
+++ support.py
@@ -6,7 +6,7 @@ from keyboards import support_kb
 router = Router()
 
 
-@router.message(F.text == "☎️ Qo'llab-quvvatlash")
+@router.message(F.text == "☎️ 𝗤𝗼ʼ𝗹𝗹𝗮𝗯-𝗾𝘂𝘃𝘃𝗮𝘁𝗹𝗮𝘀𝗵")
 async def show_support(message: Message):
     await message.answer(
         "Savollaringiz yoki muammolaringiz bo'lsa, admin bilan bog'laning:",
