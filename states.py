ce603776c94722f21947923593d3b07ba523e01c
hojiakbarbaratboyev555-cyber <hojiakbarbaratboyev555@gmail.com>
2026-07-22 17:04:08 +0500

    Update states.py

--- states.py
+++ states.py
@@ -7,15 +7,5 @@ class TransferStates(StatesGroup):
     waiting_confirm = State()
 
 
-class TopupStates(StatesGroup):
-    waiting_amount = State()
-    waiting_screenshot = State()
-
-
-class WithdrawStates(StatesGroup):
-    waiting_amount = State()
-    waiting_card = State()
-
-
 class AdminMessageStates(StatesGroup):
     waiting_message = State()
