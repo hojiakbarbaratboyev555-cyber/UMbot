c8c2e87b7b345f39afdf30c0defbc67aa7e7edbc
hojiakbarbaratboyev555-cyber <hojiakbarbaratboyev555@gmail.com>
2026-07-23 11:38:35 +0500

    Update shop.py

--- shop.py
+++ shop.py
@@ -40,8 +40,8 @@ async def show_shop(message: Message):
 
         text = (
             f"{item['emoji']} <b>{item['label']}</b>\n\n"
-            f"Real narx: {buy_price} {CURRENCY_NAME}\n"
-            f"Sotish narxi: {sell_price} {CURRENCY_NAME}"
+            f"<quoteblock>Sotib olish narxi: {buy_price} {CURRENCY_NAME}\n"
+            f"Sotish narxi: {sell_price} {CURRENCY_NAME}</quoteblock>"
         )
         await message.answer(text, reply_markup=item_trade_kb(item["key"]))
 
